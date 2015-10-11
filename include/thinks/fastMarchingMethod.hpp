35fc5ecc259ff41d7b9e5753f9cf31c31cdcfbe1
Tommy Hinks <tommy.hinks@gmail.com>
2015-10-11 22:07:41 +0200

    add folder structure

---
+++ include/thinks/fastMarchingMethod.hpp
@@ -0,0 +1,14 @@
+#ifndef THINKS_FASTMARCHINGMETHOD_HPP_INCLUDED
+#define THINKS_FASTMARCHINGMETHOD_HPP_INCLUDED
+
+namespace thinks
+{
+namespace detail
+{
+
+} // namespace detail
+
+
+} // namespace thinks
+
+#endif // THINKS_FASTMARCHINGMETHOD_HPP_INCLUDED
